58b944ea7524499af0bac4cad01f8d406880dba5
prem nakarmi <prem nakarmi>
2017-01-31 23:33:23 -0600

    adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  counter:number = 7;
+
+  changeCounterValue(event){
+    this.counter = event;
+  }
 }
